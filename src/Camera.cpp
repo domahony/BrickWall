54cfe3c108f570209a8c7a7bea2b2e49191d18cf
David O'Mahony <domahony@compusult.net>
2015-06-10 18:43:17 -0230

    Partially fixed one part of rotation issue.  Still not getting the axis of rotation correct.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -17,7 +17,7 @@ Camera::Camera():
 		eye(9.0f, 32.0f, -9.0f),
 		center(0.f, 0.f, 0.f),
 		upv(0.f, 1.f, 0.f),
-	_k(-1.f * glm::normalize(center - eye)),
+	_k(glm::normalize(eye - center)),
 	_i(glm::normalize(glm::cross(glm::normalize(upv), _k))),
 	_j(glm::normalize(glm::cross(_k, _i)))
 {
@@ -30,26 +30,36 @@ Camera::~Camera() {
 
 void Camera::
 left() {
-	glm::quat rotation(1.f * glm::pi<float>() / 360.0f, _j);
+	float c = std::cos(0.5f * glm::pi<float>() / 180.0f);
+	float s = std::sin(0.5f * glm::pi<float>() / 180.0f);
+	glm::quat rotation(c, s * _k);
 	orientation = glm::normalize(rotation * orientation);
 }
 
 void Camera::
 right() {
-	glm::quat rotation(-1.f * glm::pi<float>() / 360.0f, _j);
+	float c = std::cos(-0.5f * glm::pi<float>() / 180.0f);
+	float s = std::sin(-0.5f * glm::pi<float>() / 180.0f);
+	glm::quat rotation(c, s * _k);
 	orientation = glm::normalize(rotation * orientation);
 }
 
 void Camera::
 up() {
-	glm::quat rotation(-1.f * glm::pi<float>() / 360.0f, _i);
+	float c = std::cos(-0.5f * glm::pi<glm::vec3::value_type>() / 180.0f);
+	float s = std::sin(-0.5f * glm::pi<glm::vec3::value_type>() / 180.0f);
+	glm::quat rotation(c, s * _j);
 	orientation = glm::normalize(rotation * orientation);
+	//_i = glm::pitch(orientation);
 }
 
 void Camera::
 down() {
-	glm::quat rotation(1.f * glm::pi<float>() / 360.0f, _i);
+	float c = std::cos(0.5f * glm::pi<float>() / 180.0f);
+	float s = std::sin(0.5f * glm::pi<float>() / 180.0f);
+	glm::quat rotation(c, s * _j);
 	orientation = glm::normalize(rotation * orientation);
+	//_i = glm::pitch(orientation);
 }
 
 } /* namespace app */
