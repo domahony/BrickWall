02841825d0adfb9814193238702dd40f8b8a047e
David O'Mahony <domahony@compusult.net>
2015-07-18 18:18:18 -0230

    Making a corner.  More work needed.

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -21,6 +21,17 @@ Maze(std::shared_ptr<app::World> w) :
 	shape(floor.getShape<btBvhTriangleMeshShape>()),
 	world(w)
 {
+	float width = 20;
+	float length = 20;
+	btVector3 halfX(width/2.f, 0, 0);
+	btVector3 halfY(0, width/2.f, 0);
+	btVector3 halfZ(0, 0, width/2.f);
+
+	btQuaternion ZP90(btVector3(0, 0, 1), glm::radians(90.f));
+	btQuaternion ZN90(btVector3(0, 0, 1), glm::radians(-90.f));
+	btQuaternion XN90(btVector3(1, 0, 0), glm::radians(-90.f));
+	btQuaternion XP90(btVector3(1, 0, 0), glm::radians(90.f));
+
 	btRigidBody::btRigidBodyConstructionInfo info(0, nullptr, nullptr);
 
 	btTransform tran(btTransform::getIdentity());
@@ -28,6 +39,47 @@ Maze(std::shared_ptr<app::World> w) :
 	world->addToWorld(s, info);
 	slabs.push_back(s);
 
+	btTransform l(ZP90, tran.getOrigin() + halfX + halfY);
+	s = std::make_shared<app::gl::Slab>(mesh, shape, l);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+	l = btTransform(ZN90, tran.getOrigin() - halfX + halfY);
+	s = std::make_shared<app::gl::Slab>(mesh, shape, l);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+
+	tran.setOrigin(tran.getOrigin() + btVector3(0, 0, -width));
+	s = std::make_shared<app::gl::Slab>(mesh, shape, tran);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+	l = btTransform(ZP90, tran.getOrigin() + halfX + halfY);
+	s = std::make_shared<app::gl::Slab>(mesh, shape, l);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+	l = btTransform(XP90, tran.getOrigin() - halfZ + halfY);
+	s = std::make_shared<app::gl::Slab>(mesh, shape, l);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+	tran.setOrigin(tran.getOrigin() + btVector3(-width, 0, 0));
+	s = std::make_shared<app::gl::Slab>(mesh, shape, tran);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+	l = btTransform(XP90, tran.getOrigin() - halfZ + halfY);
+	s = std::make_shared<app::gl::Slab>(mesh, shape, l);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+
+	l = btTransform(XN90, tran.getOrigin() + halfZ + halfY);
+	s = std::make_shared<app::gl::Slab>(mesh, shape, l);
+	world->addToWorld(s, info);
+	slabs.push_back(s);
+	/*
 	tran.setOrigin(tran.getOrigin() + btVector3(20, 0, 0));
 	s = std::make_shared<app::gl::Slab>(mesh, shape, tran);
 	world->addToWorld(s, info);
@@ -72,6 +124,7 @@ Maze(std::shared_ptr<app::World> w) :
 	s = std::make_shared<app::gl::Slab>(mesh, shape, tran);
 	world->addToWorld(s, info);
 	slabs.push_back(s);
+	*/
 }
 
 void Maze::
